d2dcbdda400be329023619994bba17e81c0ce12c
ashimkarki7 <35464231+ashimkarki7@users.noreply.github.com>
2024-05-13 23:07:11 +0545

    Initalized backend and mock data and port configured
    data has mock json format and listener to start the port and api is send to front end once port is running in 500 in console.log and api routes provided

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,23 @@
+import express from 'express';
+//esmodule so we have to use . / for your own module
+import products from "./data/products.js";
+
+const port = 5000;
+ const app =express();
+
+ app.get('/', (req,res)=>{
+     console?.log('f',req,res);
+     res.send('API is running');
+ });
+
+ app.get('/api/products', (req,res)=>{
+     res.json(products);
+ })
+
+
+//list on port above
+app.listen(port, ()=>console.log('running 600'))
+
+// //common js syntax
+// const express = require('express');
 console.log('hy')
